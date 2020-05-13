21e55f9bd3fe7ad5e3eadaf2de3f271d35139ed3
Andrew Platt <andrewthomasplatt7@gmail.com>
2020-05-13 17:33:55 +0100

    added statistics for the w vector itself

--- test_lsq.m
+++ test_lsq.m
@@ -1,5 +1,6 @@
 % Get our dataset
 D = get_dataset();
+n_vars = size(D, 2) - 1;
 
 % Define the different values of frac we'll be testing
 step_size = 0.1;
@@ -7,23 +8,28 @@ fracs = 0.2:step_size:0.9;
 
 % Define how many times we should repeat for each frac, and create storage
 % vectors for the values
-n_iters = 1000;
-mean_sq_errors = zeros(length(fracs), n_iters);
+n_iters = 10;
+mean_sq_errors = zeros(n_iters, 1);
+ws = zeros(n_vars, n_iters);
 
 % And the statistics for these:
 mean_mse = zeros(length(fracs), 1);
 sd_mse = mean_mse;
+mean_w = zeros(n_vars, length(fracs));
+sd_w = mean_w;
 
 % Run the tests
 for i = 1:length(fracs)
     for j = 1:n_iters
        [train_D, test_D] = random_split(D, fracs(i)); 
-       w = lsq_regression(train_D);
-       mean_sq_errors(i, j) = compute_mean_squared_error(test_D, w);
+       ws(:,j) = lsq_regression(train_D);
+       mean_sq_errors(j) = compute_mean_squared_error(test_D, ws(:,j));
     end
     % Calculate the statistics
-    mean_mse(i) = mean(mean_sq_errors(i,:));
-    sd_mse(i) = std(mean_sq_errors(i,:));
+    mean_mse(i) = mean(mean_sq_errors);
+    sd_mse(i) = std(mean_sq_errors);
+    mean_w(:,i) = mean(ws, 2);
+    sd_w(:,i) = std(ws, 0, 2);
 end
 
 
