361f7a438c498a7653a86f5467077a227330006c
Andrew Platt <andrewthomasplatt7@gmail.com>
2020-05-15 18:34:36 +0100

    started on the ridge regression.

---
+++ ridge/ridge_regression.m
@@ -0,0 +1,21 @@
+function w = ridge_regression(train_D, lambda)
+    [n_data, n_params] = size(train_D);
+    
+    % Get the ridge offset to penalise large terms
+    ridge = n_data * lambda/2 * eye(n_params)
+    
+    % Solve as Aw = b
+    A = zeros(n_params-1);
+    b = zeros(n_params-1, 1);
+    
+    % Get A and b
+    for i = 1:n_data
+        result = train_D(i,end);
+        params = train_D(i,1:end-1)';   % Parameter values as a column vector
+        A = A + params*params';         % Outer product
+        b = b + result*params;
+    end
+    
+    
+    
+end
