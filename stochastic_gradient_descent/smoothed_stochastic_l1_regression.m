3d365444f9dadd6b8f085be917a0b8c6c47e2594
Andrew Platt <andrewthomasplatt7@gmail.com>
2020-06-08 12:43:16 +0100

    Updated regression function and tester. Obtained preliminary results.

--- stochastic_gradient_descent/smoothed_stochastic_l1_regression.m
+++ stochastic_gradient_descent/smoothed_stochastic_l1_regression.m
@@ -1,9 +1,9 @@
-function w = smoothed_stochastic_l1_regression(train_D, lambda, batch_size)
+function w = smoothed_stochastic_l1_regression(train_D, lambda, B)
     tau = 1e-2;                   % Smoothing parameter
     eta = 1;                      % Step size for gradient descent
     
     % Criteria for the loop to end
-    max_n_iterations = 1e4;
+    max_n_iterations = 1e5/B;
     
     % Parameters obtained from the training matrix
     n_params = size(train_D, 2) - 1;
@@ -11,38 +11,34 @@ function w = smoothed_stochastic_l1_regression(train_D, lambda, batch_size)
     
     % Initialisation of the w vector
     w = randn(n_params, 1);
-    prev_w = w;
     best_w = w;
     
     % Get the first loss function value and store it ready for comparison
     % with the next one and the best one
     current_loss = get_loss(train_D, w, lambda, tau);
-    prev_loss = current_loss;
     best_loss = current_loss;
     mae = zeros(max_n_iterations, 1);
-    mod_grad = zeros(max_n_iterations, 1);
-    mod_grad(1) = 1;
+    losses = zeros(max_n_iterations, 1);
     
     % Iterate and travel downhill until the loss function is small enough
     iter = 1;
     while iter < max_n_iterations
+        tau = 1e-2/iter;
         mae(iter) = compute_mean_abs_error(train_D, w);
-        prev_w = w;
-        prev_loss = current_loss;
+        losses(iter) = current_loss;
         
         % Pick a random subset of the data to use for this iteration and
         % calculate the gradient with it
-        [grad_calc_D, ~] = random_split(train_D, batch_size/n_data);
+        [grad_calc_D, ~] = random_split(train_D, B/n_data);
         grad = get_grad(grad_calc_D, w, lambda, tau);
-        mod_grad(iter) = norm(grad);
         
         % Update step size 
         eta = 1/iter;
         % Step downhill
-        w = prev_w - grad*eta;
+        w = w - grad*eta;
         current_loss = get_loss(train_D, w, lambda, tau);
         if mod(iter, 1000) == 1
-            fprintf('\x03bb: %.1g  iter: %d\tgradient magnitude: %.5g\tLoss: %.5g\teta: %.2g\n', lambda, iter, mod_grad(iter), current_loss, eta)
+            fprintf('lambda: %.1e\tB.S.: %.2g  iter: %d\tLoss: %.5g\teta: %.2g\n', lambda, B, iter, current_loss, eta)
         end
         % If our new w is the best then set it as such.
         if current_loss < best_loss
@@ -53,17 +49,17 @@ function w = smoothed_stochastic_l1_regression(train_D, lambda, batch_size)
     end
     
     w = best_w;
-    fprintf('\x03bb: %.1g  iter: %d\tgradient magnitude: %.5g\tLoss: %.5g\teta: %.2g\n', lambda, iter, mod_grad(iter), current_loss, eta)
+    fprintf('lambda: %.1e\tB.S.: %.2g  iter: %d\tLoss: %.5g\teta: %.2g\n', lambda, B, iter, current_loss, eta)
     
     % plot the error
     mae = mae(1:iter-1);
-    mod_grad = mod_grad(1:iter-1);
+    losses = losses(1:iter-1);
     subplot(2,1,1)
     plot(mae)
     xlabel('Iteration')
     ylabel('Mean Absolute Error')
     subplot(2,1,2)
-    plot(mod_grad)
-    ylabel('Gradient Magnitude')
+    plot(losses)
+    ylabel('Loss')
     drawnow
 end
--- stochastic_gradient_descent/test_stochastic_gd.m
+++ stochastic_gradient_descent/test_stochastic_gd.m
@@ -1,8 +1,8 @@
 clear variables
 frac = 0.7;
 frac2 = 0.8;
-lambda = 1e-3;
-batch_sizes = [1, 5, 10, 20];
+lambdas = [1e-3, 1e-2, 1e-1, 1];
+batch_sizes = [1, 2, 4, 8, 16, 32, 64];
 n_iterations = 10;     % Number of times to average for the final mean squared error
 iteration_mae = zeros(n_iterations, 1);
 maes = zeros(length(batch_sizes), 1);
@@ -44,19 +44,53 @@ for i = 1:length(batch_sizes)
             train_D = [sect_1; sect_2; sect_3; sect_4];
             val_D = sect_5;
         end
-        % Get the w vector and calculate the error for this value of lambda
-        w = smoothed_stochastic_l1_regression(train_D, lambda, batch_sizes(i));
+        % Get the w vector and calculate the error for this value of batch
+        % size and default val of lambda
+        w = smoothed_stochastic_l1_regression(train_D, lambdas(1), batch_sizes(i));
         section_mae(j) = compute_mean_abs_error(val_D, w);
     end
     maes(i) = mean(section_mae);
 end
 
 % Choose the best value for batch size
-[~, index] = min(maes);
+[~, b_index] = min(maes);
+B = batch_sizes(b_index);
+
+% Choose the best number for lambda using this batch size
+for i = 1:length(lambdas)
+    for j = 1:n_sections
+        if j == 1
+            train_D = [sect_2; sect_3; sect_4; sect_5];
+            val_D = sect_1;
+        elseif j == 2
+            train_D = [sect_1; sect_3; sect_4; sect_5];
+            val_D = sect_2;
+        elseif j == 3
+            train_D = [sect_1; sect_2; sect_4; sect_5];
+            val_D = sect_3;
+        elseif j == 4
+            train_D = [sect_1; sect_2; sect_3; sect_5];
+            val_D = sect_4;
+        else
+            train_D = [sect_1; sect_2; sect_3; sect_4];
+            val_D = sect_5;
+        end
+        % Get the w vector and calculate the error for this value of batch
+        % size and lambda
+        w = smoothed_stochastic_l1_regression(train_D, lambdas(i), B);
+        section_mae(j) = compute_mean_abs_error(val_D, w);
+    end
+    maes(i) = mean(section_mae);
+end
+
+% Choose the best value for batch size
+[~, l_index] = min(maes);
+lambda = lambdas(l_index);
+
 iteration_w = zeros(n_params, n_iterations);
 for i = 1:n_iterations
 %     [trainval_D, test_D] = random_split(D, frac);
-    iteration_w(:, i) = smoothed_stochastic_l1_regression(trainval_D, lambda, batch_sizes(index));
+    iteration_w(:, i) = smoothed_stochastic_l1_regression(trainval_D, lambda, B);
     iteration_mae(i) = compute_mean_abs_error(test_D, iteration_w(:, i));
 end
 w = mean(iteration_w, 2);
@@ -67,7 +101,7 @@ formatSpec = "Using a lambda value of %g and batch size of %d we obtain a w vect
 wSpec = "%.3g\n";
 maeSpec = "Which results in a Mean Absolute Error of: %.3g";
 fileID = fopen('smoothed_stochastic_l1_results.txt', 'w');
-fprintf(fileID, formatSpec, lambda, batch_sizes(index));
+fprintf(fileID, formatSpec, lambda, B);
 fprintf(fileID, wSpec, w);
 fprintf(fileID, maeSpec, mae);
 fclose('all');
